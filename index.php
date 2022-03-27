35d3ed9465fe719f47a9c36e856bb02da7b1f6e5
szwedzius <jakubszweda1@gmail.com>
2022-03-27 20:16:31 +0200

    Frontend for the main menu

--- index.php
+++ index.php
@@ -16,10 +16,29 @@
                 <div id="con" class="col-sm-12 col-xl-4 col-12">Contact: 700 700 700 or placeholder@gmail.com</div>
             </div>
             <div class="row">
-                <div class="col-4 col-sm-3 col-xl-2">
+                <div class="col-4 col-sm-3 col-lg-2">
                     <img src="images/logo.png" class="logo" alt="">
                 </div>
-                <div class="col-5 col-sm-6 search_bar">
+                <div class="col-sm-6 col-5 search_bar bigger_screen">
+                    <input type="text" class="search" placeholder="Look up what you want">
+                </div>
+                <div class="col-lg-2 offset-3 offset-sm-0 col-2 col-sm-4 login">
+                    <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M8.5 9.5C8.5 7.567 10.067 6 12 6C13.933 6 15.5 7.567 15.5 9.5C15.5 11.433 13.933 13 12 13C10.067 13 8.5 11.433 8.5 9.5ZM12 8C11.1716 8 10.5 8.67157 10.5 9.5C10.5 10.3284 11.1716 11 12 11C12.8284 11 13.5 10.3284 13.5 9.5C13.5 8.67157 12.8284 8 12 8Z" fill="#152C70"/>
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M15.3307 21.4319C19.2161 20.0599 22 16.355 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 16.355 4.78394 20.0599 8.66927 21.4319C9.52012 21.7843 10.611 22 12 22C13.3889 22 14.4798 21.7843 15.3307 21.4319ZM12 4C7.58172 4 4 7.58172 4 12C4 14.1106 4.8173 16.0302 6.15267 17.4598C6.93324 15.9504 8.49158 15 10.1943 15H13.806C15.5086 15 17.0669 15.9503 17.8475 17.4596C19.1828 16.0301 20 14.1105 20 12C20 7.58172 16.4183 4 12 4ZM14.6147 19.5629C15.0486 19.3753 15.3306 19.1555 15.5144 18.9634C15.791 18.6741 15.9106 18.3796 15.9621 18.1883C15.4982 17.4536 14.6865 17 13.806 17H10.1943C9.31368 17 8.50197 17.4536 8.0381 18.1883C8.08956 18.3796 8.20915 18.6742 8.48575 18.9634C8.6695 19.1555 8.95143 19.3753 9.38527 19.5629C10.2046 19.8462 11.0843 20 12 20C12.9156 20 13.7954 19.8462 14.6147 19.5629Z" fill="#152C70"/>
+                    </svg>
+                    <span class="menu_item">Login</span>
+                    
+                </div>
+                <div class="col-lg-2 col-2 col-sm-4 checkout">
+                    <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M4 3C3.44772 3 3 3.44772 3 4C3 4.55228 3.44772 5 4 5H5C5.50065 5 5.91537 5.36792 5.98854 5.84813L6.26745 10.3118C6.43211 12.947 8.61737 15 11.2577 15H16C18.7614 15 21 12.7614 21 10C21 7.23858 18.7614 5 16 5H7.82929C7.41746 3.83481 6.30622 3 5 3H4ZM8.26356 10.1871L8.06442 7H16C17.6568 7 19 8.34315 19 10C19 11.6569 17.6568 13 16 13H11.2577C9.67351 13 8.36235 11.7682 8.26356 10.1871Z" fill="#152C70"/>
+                    <path d="M12 19C12 20.1046 11.1046 21 10 21C8.89543 21 8 20.1046 8 19C8 17.8954 8.89543 17 10 17C11.1046 17 12 17.8954 12 19Z" fill="#152C70"/>
+                    <path d="M16 21C17.1046 21 18 20.1046 18 19C18 17.8954 17.1046 17 16 17C14.8954 17 14 17.8954 14 19C14 20.1046 14.8954 21 16 21Z" fill="#152C70"/>
+                    </svg>
+                    <span class="menu_item">Checkout</span>
+                </div>
+                <div class="offset-1 col-10 of search_bar smaller_screen">
                     <input type="text" class="search" placeholder="Look up what you want">
                 </div>
             </div>
@@ -29,4 +48,5 @@
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script src="main_page.js"></script>
 </html>
